9e006aa6cfbb59b688d576c569b3df775ca38962
arcuellar88 <alejandro.rodriguez88@gmail.com>
2017-01-03 09:24:04 -0500

    fix schema matching

--- R/harmonize.R
+++ R/harmonize.R
@@ -26,40 +26,129 @@ files <- list.files("./data")
 
 mecovi_list <- list()
 mecovi_vars <- list()
-
-
   for(i in 1:length(files)){
-    
-    db <-read.dta(paste0("./data/",files[i]), convert.factors=factor)
-    
-    #name of the columns
-    dbVars <- as.data.frame(colnames(db))
-    colnames(dbVars)<- c("var")
-    
-    fname = str_sub(files[i],1,-5) # remove .csv from the file name
-    
+  
+    # remove .csv from the file name
+    fname = str_sub(files[i],1,-5) 
+
     print(paste0("Reading: ",fname))
     
-    db2<-db[,t(vars)]
+    #Read dta  
+    db <-read.dta(paste0("./data/",files[i]), convert.factors=factor)
+    
+    #Create dataset of variables
+    #dbVars <- as.data.frame(colnames(db))
+    #colnames(dbVars)<- c("var")
+    
+    # Find names of missing columns
+    missing <- setdiff(vars$x, names(db))
+    
+    print(paste0("# missing columns: ",length(missing)))
+    
+    db[missing] <- NA
+    
+    db$techo_ch <- factor(db$techo_ch)
+    db$tipopen_ci <-factor(db$tipopen_ci)
+    db$instcot_ci<-factor(db$instcot_ci)
+    db$jefe_ci<-factor(db$jefe_ci)
+    db$pqnoasis_ci<-factor(db$pqnoasis_ci)
+    db$region_c<-factor(db$region_c)
+    db$edupub_ci<-factor(db$edupub_ci)
+    db$bano_ch<-factor(db$bano_ch)
+    db$aguamide_ch<-factor(db$aguamide_ch)
+    db$combust_ch<-factor(db$combust_ch)
+    db$luzmide_ch<-factor(db$luzmide_ch)
+    db$raza_idioma_ci<-factor(db$raza_idioma_ci)
+    db$resid_ch<-factor(db$resid_ch)
+    db$viviprop_ch<-factor(db$viviprop_ch)
+    db$pared_ch<-as.character(db$pared_ch)
+    db$piso_ch<-as.character(db$piso_ch)
+    db$raza_ci<-as.character(db$raza_ci)
+    
+    db<-db[,t(vars)]
     
     # creates a variable with the file name
-    db2$survey = rep(fname, nrow(db)) 
-    dbVars$survey=rep(fname, nrow(dbVars)) 
+    db$survey = rep(fname, nrow(db)) 
+    #dbVars$survey=rep(fname, nrow(dbVars)) 
+  
+  
+    mecovi_list[[i]] = db  
+    #mecovi_vars[[i]] = dbVars  
     
+    #if(i==1){
+     # write.csv(db, file="full_data_FACTOR.csv", sep=",", col.names=TRUE, append=FALSE, row.names=FALSE, quote=TRUE, na="", fileEncoding="UTF-8")
+    #}
+  #  else {
+   #   write.csv(db, file="full_data_FACTOR.csv", sep=",", col.names=FALSE, append=TRUE, row.names=FALSE, quote=TRUE, na="", fileEncoding="UTF-8")  
+    # } 
     
-    
-    mecovi_list[[i]] = db2  
-    mecovi_vars[[i]] = dbVars  
-    
-    #write.table(db2, file=paste0(fname,".csv"), sep=",", col.names=TRUE, row.names=FALSE, quote=TRUE, na="", fileEncoding="UTF-8")
   }
 
+mecovi_list
 #print("Starting: bind rows")
 #full_data <- bind_rows(mecovi_list) 
 #full_vars <- bind_rows(mecovi_vars) 
 
 #write.table(full_data, file="full_data_FACTOR.csv", sep=",", col.names=TRUE, row.names=FALSE, quote=TRUE, na="", fileEncoding="UTF-8")
 #write.table(full_vars, file="full_vars.csv", sep=",", col.names=TRUE, row.names=FALSE, quote=TRUE, na="", fileEncoding="UTF-8")
-mecovi_list
+#full_data
 
+}
+
+
+check_metadata <- function(mecovi_list)
+{
+  metadata <- list()
+  for(i in 1:length(mecovi_list)){
+    meta=as.data.frame(sapply(mecovi_list[[i]], class))
+    colnames(meta)=c(paste0("type",i))
+    meta <- add_rownames(meta, "col")
+    meta <- meta[order(meta$col),] 
+    
+    metadata[[i]]=meta
+  }
+  metadata
+}
+
+join_meta<-function(meta_list)
+{
+  meta<-''
+  for(i in 1:length(meta_list)){
+    if (i== 1){
+      meta=meta_list[[i]]
+    } 
+    else
+    {
+      meta<-inner_join(meta,meta_list[[i]],by="col")
+      #meta<-merge(meta, meta_list[[i]], by=c("col")) # NA's match
+      
+    }
+  }
+  meta
+  
+}
+#http://stackoverflow.com/questions/2851015/convert-data-frame-columns-from-factors-to-characters/2853231#2853231
+
+as_character_df<-function(mecovi_list)
+{
+  for(i in 1:length(mecovi_list)){
+    db<-mecovi_list[[i]]
+    x <- sapply(db, is.factor)
+    db[x] <- lapply(db[x], as.character)
+    mecovi_list[[i]] <-db
+  }
+  mecovi_list
+}
+
+printDF<-function(mecovi_list)
+{
+  for(i in 1:3){
+    if(i==1){
+      write.table(mecovi_list[[i]], file="full_data_FACTOR.csv", sep=",", col.names=TRUE, append=FALSE, row.names=FALSE, quote=TRUE, na="", fileEncoding="UTF-8")
+    }
+    else {
+      write.table(mecovi_list[[i]], file="full_data_FACTOR.csv", sep=",", col.names=FALSE, append=TRUE, row.names=FALSE, quote=TRUE, na="", fileEncoding="UTF-8")  
+    } 
+  }
+  
 }
